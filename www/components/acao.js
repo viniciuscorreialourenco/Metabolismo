3e08ad744ddd6ef890be5344a17b29b3b7d62e76
vlvkvinicius <lourenco_vinicius@yahoo.com>
2019-03-15 15:29:58 +0900

    adicionando as funções do jquery!

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,15 +1,75 @@
 // This is a JavaScript file
 
-var ps;
-var al;
-var id;
-var sx;
-var atv;
 var res;
 
-$document.on("click" , "#calcular" , function(){
+$(document).on("click","#calcular", function(){
   ps = $("#peso").val()
   al = $("#altura").val()
   id = $("#idade").val()
-  sx = $("#sexo").val()
+  
+  if($("#sexo1").is(":checked")){
+
+    if($("#sedentario").is(":selected")){
+
+        res=1.2*(66+(13.7*ps)+(5*al)-(6.8*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+      
+    }
+    else if($("#levemente").is(":selected")){
+
+      res=1.375*(66+(13.7*ps)+(5*al)-(6.8*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+    else if($("#moderadamente").is(":selected")){
+
+      res=1.55*(66+(13.7*ps)+(5*al)-(6.8*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+    else if($("#altamente").is(":selected")){
+
+      res=1.725*(66+(13.7*ps)+(5*al)-(6.8*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+    else if($("#extremamente").is(":selected")){
+
+      res=1.9*(66+(13.7*ps)+(5*al)-(6.8*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+  }
+  else if($("#sexo2").is(":checked")){
+    if($("#sedentario").is(":selected")){
+
+        res=1.2*(655+(9.6*ps)+(1.8*al)-(4.7*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+      
+    }
+    else if($("#levemente").is(":selected")){
+
+      res=1.375*(655+(9.6*ps)+(1.8*al)-(4.7*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+    else if($("#moderadamente").is(":selected")){
+
+      res=1.55*(655+(9.6*ps)+(1.8*al)-(4.7*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+    else if($("#altamente").is(":selected")){
+
+      res=1.725*(655+(9.6*ps)+(1.8*al)-(4.7*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+    else if($("#extremamente").is(":selected")){
+
+      res=1.9*(655+(9.6*ps)+(1.8*al)-(4.7*id));
+        $("#resultado").val(res.toFixed(2)+" (kcal)");
+
+    }
+  }
 });
